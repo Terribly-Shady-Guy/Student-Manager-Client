f3f2fdad62f5d52532130f60e823e78b6b55de16
Terribly-Shady-Guy <tylerkaufmann98@gmail.com>
2023-03-25 15:24:45 -0500

    added react query and react hook form

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import './App.css'
 import Navigation from './components/Navigation'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 function App() {
+  const client = new QueryClient();
+
   return (
     <div className="App">
       <header>
         <h1>Buffalo College</h1>
       </header>
-      <Navigation />
+      <QueryClientProvider client={client}>
+        <Navigation />
+      </QueryClientProvider>
       <footer>
         <p>Test</p>
       </footer>
--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
 
 export default function LoginForm() {
+  const { register, handleSubmit } = useForm();
+
+  function onSubmit(data) {
+    console.log(data);
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <label htmlFor='username'>Username: </label>
-      <input type="text" name="username" id="username" />
+      <input type="text" {...register("username", { require: true})} id="username" />
       <label htmlFor='password'>Password: </label>
-      <input type="password" name="password" id="password" />
-      <button type="button">Login</button>
+      <input type="password" {...register("password", { require: true})} id="password" />
+      <button type="submit">Login</button>
     </form>
   )
 }
