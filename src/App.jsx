a3ba25d4c9308ebf96b6077b8120b97934aac132
Terribly-Shady-Guy <tylerkaufmann98@gmail.com>
2023-03-25 09:38:54 -0500

    moved navigation code to new component

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,10 @@
 import './App.css'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
-import Login from "./pages/Login"
-import SignUp from './pages/SignUp'
-import StudentForm from './pages/StudentForm'
-import StudentRegistrationList from './pages/StudentRegistrationList'
+import Navigatiom from './components/Navigatiom'
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <nav>
-          <Link to='/login'>Login</Link>
-          <Link to='/signup'>Sign Up</Link>
-          <Link to='/studentform'>Register</Link>
-          <Link to='/studentregistrationlist'>Student Registrations</Link>
-        </nav>
-        <Routes>
-          <Route path='/login' element={<Login />} />
-          <Route path='/signup' element={<SignUp />} />
-          <Route path='/studentform' element={<StudentForm />} />
-          <Route path='/studentregistrationlist' element={<StudentRegistrationList />} />
-        </Routes>
-      </Router>
+      <Navigatiom />
     </div>
   )
 }
