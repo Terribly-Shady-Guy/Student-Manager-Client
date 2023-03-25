6dfa2b82767d1dcdafb165fd4233e7de2199357a
Terribly-Shady-Guy <tylerkaufmann98@gmail.com>
2023-03-25 11:09:41 -0500

    added home view component and fixed nav code formatting

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Home from '../pages/Home'
 import Login from "../pages/Login"
 import SignUp from '../pages/SignUp'
 import StudentForm from '../pages/StudentForm'
@@ -8,18 +9,20 @@ import StudentRegistrationList from '../pages/StudentRegistrationList'
 export default function Navigation() {
   return (
     <Router>
-        <nav>
-            <Link to='/login'>Login</Link>
-            <Link to='/signup'>Sign Up</Link>
-            <Link to='/studentform'>Register</Link>
-            <Link to='/studentregistrationlist'>Student Registrations</Link>
-        </nav>
-            <Routes>
-            <Route path='/login' element={<Login />} />
-            <Route path='/signup' element={<SignUp />} />
-            <Route path='/studentform' element={<StudentForm />} />
-            <Route path='/studentregistrationlist' element={<StudentRegistrationList />} />
-        </Routes>
-  </Router>
+      <nav>
+        <Link to='/'>Home</Link>
+        <Link to='/login'>Login</Link>
+        <Link to='/signup'>Sign Up</Link>
+        <Link to='/studentform'>Register</Link>
+        <Link to='/studentregistrationlist'>Student Registrations</Link>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/studentform' element={<StudentForm />} />
+        <Route path='/studentregistrationlist' element={<StudentRegistrationList />} />
+      </Routes>
+    </Router>
   )
 }
