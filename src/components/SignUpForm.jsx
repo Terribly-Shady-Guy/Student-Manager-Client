6844e3d455ab824c68cb1ac5fd2fa05b1749fe99
Terribly-Shady-Guy <tylerkaufmann98@gmail.com>
2023-03-25 11:30:34 -0500

    added text to sign up labels

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -3,15 +3,15 @@ import React from 'react'
 export default function SignUpForm() {
   return (
     <form>
-        <label htmlFor="first-name"></label>
+        <label htmlFor="first-name">First Name: </label>
         <input type="text" name="firstName" id="first-name" />
-        <label htmlFor="last-name"></label>
+        <label htmlFor="last-name">Last Name: </label>
         <input type="text" name="lastName" id="last-name" />
-        <label htmlFor="email"></label>
+        <label htmlFor="email">Email: </label>
         <input type="email" name="email" id="email" />
-        <label htmlFor="username"></label>
+        <label htmlFor="username">Username: </label>
         <input type="text" name="username" id="username" />
-        <label htmlFor="password"></label>
+        <label htmlFor="password">Password: </label>
         <input type="password" name="password" id="password" />
         <button type='button'>Create</button>
     </form>
