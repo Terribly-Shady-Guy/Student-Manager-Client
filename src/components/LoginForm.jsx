bb97ef27e0d4f4563981ad84c58588fd44fff329
Terribly-Shady-Guy <tylerkaufmann98@gmail.com>
2023-03-25 14:51:05 -0500

    fixed code formatting

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 export default function LoginForm() {
   return (
     <form>
-        <label htmlFor='username'>Username: </label>
-        <input type="text" name="username" id="username" />
-        <label htmlFor='password'>Password: </label>
-        <input type="password" name="password" id="password" />
-        <button type="button">Login</button>
+      <label htmlFor='username'>Username: </label>
+      <input type="text" name="username" id="username" />
+      <label htmlFor='password'>Password: </label>
+      <input type="password" name="password" id="password" />
+      <button type="button">Login</button>
     </form>
   )
 }
