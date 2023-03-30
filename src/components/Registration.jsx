ef3a1c7113d48c2c38fea4bc2df4ed9719a996c0
Terribly-Shady-Guy <tylerkaufmann98@gmail.com>
2023-03-29 19:45:43 -0500

    moved tables into Registration for reusabiity

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
 
 export default function Registration({props}) {
-  return (
-    <tr>
-      <td>{props.courseNumber}</td>
-      <td>{props.credits}</td>
-      <td>{props.attendenceType}</td>
-      <td>{props.bookFormat}</td>
+  const registrations = props.registrations.map((item, index) => (
+    <tr key={index}>
+      <td>{item.courseNumber}</td>
+      <td>{item.credits}</td>
+      <td>{item.attendenceType}</td>
+      <td>{item.bookFormat}</td>
     </tr>
+  ));
+
+  return (
+    <table>
+    <thead>
+      <tr>
+        <th>Course #</th>
+        <th>Credits</th>
+        <th>Attendence Type</th>
+        <th>Book Format</th>
+      </tr>
+    </thead>
+    <tbody>
+      {registrations}
+    </tbody>
+  </table>
   )
 }
