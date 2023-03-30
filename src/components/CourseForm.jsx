4d1d165b8a66b430b335904d69ded52f70a3296a
Terribly-Shady-Guy <tylerkaufmann98@gmail.com>
2023-03-29 19:25:07 -0500

    added code to submit course

--- src/components/CourseForm.jsx
+++ src/components/CourseForm.jsx
@@ -1,18 +1,33 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
 
 export default function CourseForm() {
+  const { register, handleSubmit } = useForm();
+
+  const submitForm = async (course) => {
+    await fetch("http://localhost:5119/api/courses", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        Authorization: `Bearer ${window.localStorage.getItem("accessToken")}`
+      },
+      body: JSON.stringify(course)
+    });
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(submitForm)}>
       <label htmlFor="input-course-number">Course Number: </label>
-      <input type="text" name="courseNumber" id="input-course-number" />
+      <input type="text" {...register("courseNumber")} id="input-course-number" />
       <label htmlFor="input-course-name">Course Name: </label>
-      <input type="text" name="courseName" id="input-course-name" />
+      <input type="text" {...register("courseName")} id="input-course-name" />
       <label htmlFor="input-instructor">Instructor: </label>
-      <input type="text" name="instructor" id="input-instructor" />
+      <input type="text" {...register("instructor")} id="input-instructor" />
       <label htmlFor="date-start-date">Start Date: </label>
-      <input type="date" id="date-start-date" name="startDate" />
-      <label htmlFor="date-end-date">End Date: </label><input type="date" name="endDate" id="date-end-date" />
-      <button type='button'>Add Course</button>
+      <input type="date" {...register("startDate")} id="date-start-date" />
+      <label htmlFor="date-end-date">End Date: </label>
+      <input type="date" {...register("endDate")} id="date-end-date" />
+      <button type='submit'>Add Course</button>
     </form>
   )
 }
